d3ac96e4eae37294ee0ba35daf23421b7482e6b2
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-07-11 20:07:56 -0700

    rm whitespace

--- src/dancer.js
+++ src/dancer.js
@@ -4,14 +4,6 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
 
-  // use jQuery to create an HTML <span> tag
-  // this.$node = $('<span class="dancer"></span>');
-  
-  // this.step();
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  // this.setPosition(top, left);
 };
 
 makeDancer.prototype.step = function() {
