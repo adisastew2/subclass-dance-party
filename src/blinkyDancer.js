deb7035a2eccf354f163eec1955ab83e31baefd6
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-07-11 19:20:42 -0700

    get rid of whitespace

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,9 +4,6 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.step();
   this.setPosition(top, left);
-
-
-
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
