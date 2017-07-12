321de3998f5137a22cdd4a3c816b5558f5d6e473
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-07-11 18:47:55 -0700

    refactor blinkydancer to psuedoclassical instantitation

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,9 +1,11 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  // var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
-  makeDancer.call(this);
-  this.top = top;
-  this.left = left;
-  this.timeBetweenSteps = timeBetweenSteps;
+  
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="dancer"></span>');
+  this.step();
+  this.setPosition(top, left);
+
+
 
 };
 
@@ -11,12 +13,9 @@ makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    
-  
+  // call the old version of step at the beginning of any call to this new version of step
+     
   makeDancer.prototype.step.call(this, this.timeBetweenSteps);
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
+    
   this.$node.toggle();
 };
